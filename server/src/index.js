4b6d2e5ecfbf795989020aac7b8034056e5c943b
Dhruv <Dhruv262004@gmail.com>
2025-04-13 12:25:42 +0530

    initialzed the frontend and made login and Register page

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Smart Waste Management API is running");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
